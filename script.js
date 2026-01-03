435d611f543daca5423e52a0a5b2804ffc71a3fd
The Ryoka <sairenyaa@gmail.com>
2026-01-03 11:33:40 +0100

    Auto stash before rebase of "main"

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+const rates = {
+    EUR: 10,
+    USD: 20,
+    GBP: 25,
+    UAN: 41,
+    PLN: 11
+};
+
+const amountimput = document.getElementById("amount");
+const fromselect = document.getElementById("from");
+const fromto = document.getElementById("to");
+const convertbutton = document.getElementById("convert");
+
