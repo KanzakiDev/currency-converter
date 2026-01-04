65aef293012330708d132d20ef8db7e56927f3ef
The Ryoka <sairenyaa@gmail.com>
2026-01-04 09:34:24 +0100

    script

--- script.js
+++ script.js
@@ -10,7 +10,23 @@ const amountimput = document.getElementById("amount");
 const fromselect = document.getElementById("from");
 const fromto = document.getElementById("to");
 const convertbutton = document.getElementById("convert");
+const resultText = document.getElementById("result");
+
 
 convertbutton.addEventListener("click", () => {
-    console.log("click"); 
+    convertfunc();
 });
+
+function convertfunc() {
+    const amountnumbres = parseFloat(amountimput.value);
+    const from = fromselect.value;
+    const to = fromto.value;
+    
+    if (isNaN(amountnumbres) || amountnumbres <= 0) {
+        alert("Please enter a valid amount greater than zero.");
+        return;
+    }
+    result = amount * rates[to] / rates[from];
+
+    resultText.textContent = `Result: ${result.toFixed(2)} ${to}`;
+}
